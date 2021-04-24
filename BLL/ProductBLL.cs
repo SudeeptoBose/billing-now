f1dca12c804fef7919c058628fcda1e6e172683c
SudeeptoBose <39208459+SudeeptoBose@users.noreply.github.com>
2021-04-25 00:13:34 +0600

    Add files via upload

---
+++ BLL/ProductBLL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingNowDemo.BLL
+{
+    class ProductBLL
+    {
+        public int productID { get; set; }
+        public string productName { get; set; }
+        public decimal price { get; set; }
+    }
+}
